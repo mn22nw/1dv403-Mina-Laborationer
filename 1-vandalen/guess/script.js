1f42b1271f4fb17742d3247fe9c705b0cec1d233
mn22nw <mn22nw@student.lnu.se>
2013-11-26 02:19:55 +0100

    Hemliga talet

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,27 +2,36 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var secret = 5;//Math.floor(Math.random() * 100 + 1);
+	var clicks = 0;
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
-
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		
+		clicks +=1; 
+		var array =[];
+		array[0]=[true, "Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+clicks+" gissningar för att hitta det."];
+		array[1]=[false, "Det hemliga talet är högre!"];
+		array[2]=[false, "Det hemliga talet är lägre!"];
+		array[3]=[false, "Talet är utanför intervallet 0 - 100"];
+		
+		if ((+number )=== secret){
+		return (array[0]);
+		}
+		if ((+number ) >100 || (+number ) <0 ){
+		return (array[3]);
+		}
+		if ((+number )< secret){
+		return (array[1]);
+		}
+		if ((+number )> secret){
+		return (array[2]);
+		}
+	
 	};
 	
 	// ------------------------------------------------------------------------------
-
-
-
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
 	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
 	var input = document.querySelector("#number");
@@ -34,10 +43,10 @@ window.onload = function(){
 
 		var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
 		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
-
+		
+		
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
 			submit.disabled = true;
 		}
-	
 	});
 };
