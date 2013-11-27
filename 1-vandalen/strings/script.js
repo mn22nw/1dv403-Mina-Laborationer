92b0f7f6768f30ddbd7a99c9bd76a84e44573d86
mn22nw <mn22nw@student.lnu.se>
2013-11-27 20:58:07 +0100

    Nästan klar med Strings

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -2,43 +2,49 @@
 
 window.onload = function(){
 
-	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var convertString = function(str){
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
-
-
-
-
-
-
-	};
-	// ------------------------------------------------------------------------------
-
-
-	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
-	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
-	var input = document.querySelector("#string");
-	var submit = document.querySelector("#send");
-
-	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
-	submit.addEventListener("click", function(e){
-		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
-
-		p.classList.remove( "error");
-
-		try {
-			var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
-			p.innerHTML = answer;		// Skriver ut texten från arrayen som skapats i funktionen.	
-		} catch (error){
-			p.classList.add( "error"); // Växla CSS-klass, IE10+
-			p.innerHTML = error.message;
+        // I denna funktion ska du skriva koden för att hantera "spelet"
+        var convertString = function(str){
+		strArray = new Array();
+		for (var i=0; i < str.length; i++){
+		strArray.push(str[i]);
 		}
-	
-	});
+		var checkCase = strArray;
+		for (var i =0; i <checkCase.length; i++){
+		
+		if (checkCase[i].toLowerCase() !== checkCase[i])  { 
+		checkCase[i] = checkCase[i].toLowerCase(); 
+		continue;}
+		
+		if (checkCase[i].toUpperCase() !== checkCase[i]){ 
+		checkCase[i] = checkCase[i].toUpperCase();
+		continue;	}
+		}
+		var fixedStr = checkCase.join("");
+		return fixedStr;
+// Vid fel, kasta ett undantag med ett meddelande till användaren. 
+        };
+        // ------------------------------------------------------------------------------
 
 
+        // Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
+        var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
+        var input = document.querySelector("#string");
+        var submit = document.querySelector("#send");
+
+        // Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
+        submit.addEventListener("click", function(e){
+                e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
+
+                p.classList.remove( "error");
+
+                try {
+                        var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+                        p.innerHTML = answer;                // Skriver ut texten från arrayen som skapats i funktionen.        
+                } catch (error){
+                        p.classList.add( "error"); // Växla CSS-klass, IE10+
+                        p.innerHTML = error.message;
+                }
+        
+        });
 
 };
