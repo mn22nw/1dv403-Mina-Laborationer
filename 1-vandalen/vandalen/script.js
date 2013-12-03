2761eae8f462c5eeccf6eec98c1cccf40cea2dde
mn22nw <mn22nw@student.lnu.se>
2013-12-03 03:09:32 +0100

    testen passerade

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -10,65 +10,21 @@
 	});
 	nameArray.sort(function(a,b){ return a.localeCompare(b);});
 	nameArray = nameArray.join(", "); 
-	//persArr.prototype.minAge
-
-	
-  //console.log(person1.getName());
+	ageArray.sort();
+	var total = 0;
+	ageArray.forEach(function(age) {
+	total+=age;
+	});
   
+   
   var result = {
   
-  minAge: 0,
-  maxAge:120,
-  averageAge: 67,
+  minAge:ageArray[0],
+  maxAge:ageArray[ageArray.length-1],
+  averageAge: Math.round(total/ ageArray.length),
   names:nameArray
   };
-
-  /* var objPerson = {
-
-	names: "RUMPLE",
-	minAge: 0,
-	maxAge:120,
-	averageAge: 30}*/
-
+  
    return result;
-
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-
-var result = makePerson(data);
-
-console.log(result);
-/*
-
-
-var output = “”;
-if (typeof args.name == “string”){
-output += “Name: “ + args.name + “\n”;
-}
-if (typeof args.age == “number”) {
-output += “Age: “ + args.age + “\n”;
-}
-alert(output);
-}
-displayInfo({ 
-name: “Nicholas”, 
-age: 29
-});
-displayInfo({
-name: “Greg”
-
-var objPerson = {
-
-names: "RUMPLE",
-minAge: 0;
-maxAge:120;
-averageAge: 30;
-//function(){
-//return "eehokej_kanske inte";
-
-
-return names;
-} */
-	// Din kod här...
-
 };
 
