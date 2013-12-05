c2e8d6cfe222ba04669bd816651668eed640d4e9
mn22nw <mn22nw@student.lnu.se>
2013-12-05 10:13:13 +0100

    fixar mer

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -23,4 +23,22 @@
 window.onload = function() {
 MessageBoard.init(); // kör init-funktionen// lägger till meddelande i arrayen
 console.log(MessageBoard.messages[0].getText());
+
+	// ------------------------------------------------------------------------------
+	// Kod för att hantera utskrift och inmatning
+
+	var submit = document.querySelector("#send");
+
+	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
+		submit.addEventListener("click", function(e){
+		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
+
+		var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
+		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
+		
+		
+		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
+			submit.disabled = true;
+		}
+	});
 }
