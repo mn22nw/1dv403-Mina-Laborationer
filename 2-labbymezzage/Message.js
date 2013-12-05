71b134e8c9963b75a2b8cbf626da0f2cb07ddf95
mn22nw <mn22nw@student.lnu.se>
2013-12-05 08:06:53 +0100

    Börjat fixa

---
+++ 2-labbymezzage/Message.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+	
+	function Message(message, date)
+{
+this.getDate = function () { return date; };
+this.setDate = function (_date) { date = _date; };
+
+this.getText = function () { return message; };
+this.setText = function (_text) { message = _text; };
+
+
+this.setDate(date);
+this.setText(message);
+}
+
+Message.prototype.toString = function() {
+ return this.getText()+" ("+this.getDate()+") "; }
+ 
+Message.prototype.getHTMLText = function() {
+
+// här ska det visst finnas något
+
+}
+Message.prototype.getDateText = function() {
+
+// här ska det visst också finnas något
+
+}
---
+++ 2-labbymezzage/MessageBoard.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+//window.onload = function(){
+	
+	var MessageBoard = {
+	messages: [], 
+	init: function(e) {
+	var mess = new Message ("Mia Nygren", new Date());
+	
+	console.log(mess.toString());
+	console.log(mess.getText());  // skriver ut texten
+	mess.setText("Whhaaat, en ny text?! vad säger Rumple om detta?");
+	console.log(mess.getText());
+	MessageBoard.messages.push(mess);
+	
+	}
+	}
+	/*MessageBoard.prototype.play = function() { 
+	init();
+	}
+	MessageBoard.play();*/
+	//};
+window.onload = function() {
+MessageBoard.init(); // kör init-funktionen// lägger till meddelande i arrayen
+console.log(MessageBoard.messages[0].getText());
+}
