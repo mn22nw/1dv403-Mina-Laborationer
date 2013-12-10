8f4f5647a262fc18fc1270df9bacd4fb8ba876d5
mn22nw <mn22nw@student.lnu.se>
2013-12-10 23:24:42 +0100

    småfix

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,5 +1,10 @@
 ﻿"use strict";
 
+window.onload = function() {
+
+	var div = document.querySelector("#displayMessages"); 
+	var submit = document.querySelector("#send");
+	var input = document.querySelector("#text");
 	
 	var MessageBoard = {
 	messages: [], 
@@ -10,27 +15,38 @@
 	console.log(mess.toString());
 	//mess.setText(e);
 	MessageBoard.messages.push(mess);
-	}
+	},
+	
+	renderMessage: function(messageID) {
+		var messageDiv = document.createElement("div");
+		messageDiv.className = 'displayMessage';
+		var text = document.createElement("p");
+		var date = document.createElement("p");
+		text.className = 'messageText';
+		date.className ='messageDate';
+		text.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+		date.innerHTML = MessageBoard.messages[messageID].getDateText();
+		div.appendChild(messageDiv);
+		messageDiv.appendChild(text);
+		messageDiv.appendChild(date);
+		
+		
+		}
 	}
 	
-window.onload = function() {
+
 	// ------------------------------------------------------------------------------
 	// Kod för att hantera utskrift och inmatning
-	var p = document.querySelector("#value"); 
-	if(p == "") console.log("eheh");
-	var t = document.querySelector("#time"); 
-	var submit = document.querySelector("#send");
-	var input = document.querySelector("#text");
+	
 
 	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
 		submit.addEventListener("click", function(e){
 		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
-		p.innerHTML = "";
+		div.innerHTML = "";
 		var answer = MessageBoard.init(input.value); // Läser in texten från textrutan och skickar till funktionen "guess"
 		
-		renderMessage: function(messageID) {
-		t.innerHTML  = MessageBoard.messages[messageID].getHTMLText();
-		}
+		
+		MessageBoard.renderMessage(0);
 
 		//var writeMessages = MessageBoard.messages;
 		//writeMessages.forEach(function(mesg) {
