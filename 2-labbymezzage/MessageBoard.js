afa7cee667f6001dce4ed8c400165c97ee3eeffd
mn22nw <mn22nw@student.lnu.se>
2013-12-11 01:26:57 +0100

    japp

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -5,15 +5,15 @@ window.onload = function() {
 	var div = document.querySelector("#displayMessages"); 
 	var submit = document.querySelector("#send");
 	var input = document.querySelector("#text");
+	var countMessages = document.querySelector("#countMessages");
+	countMessages.innerHTML = "Antal meddelanden"+": "+ 0;
 	
 	var MessageBoard = {
 	messages: [], 
 	init: function(e) {
 	
 	var mess = new Message (e, new Date());
-	
 	console.log(mess.toString());
-	//mess.setText(e);
 	MessageBoard.messages.push(mess);
 	},
 	
@@ -28,10 +28,19 @@ window.onload = function() {
 		date.innerHTML = MessageBoard.messages[messageID].getDateText();
 		div.appendChild(messageDiv);
 		messageDiv.appendChild(text);
-		messageDiv.appendChild(date);
-		
-		
-		}
+		messageDiv.appendChild(date); 
+		},
+	renderMessages: function() {
+	// Ta bort alla meddelanden
+	document.getElementById("displayMessages").innerHTML = "";
+	document.getElementById("text").value = "";
+	var numberOfMessages = 0;
+	// Rendera alla meddelanden
+	MessageBoard.messages.forEach(function() {
+	MessageBoard.renderMessage(numberOfMessages++);
+	});
+	countMessages.innerHTML = "Antal meddelanden"+": "+numberOfMessages;
+	}
 	}
 	
 
@@ -42,19 +51,18 @@ window.onload = function() {
 	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
 		submit.addEventListener("click", function(e){
 		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
-		div.innerHTML = "";
-		var answer = MessageBoard.init(input.value); // Läser in texten från textrutan och skickar till funktionen "guess"
-		
-		
-		MessageBoard.renderMessage(0);
-
-		//var writeMessages = MessageBoard.messages;
-		//writeMessages.forEach(function(mesg) {
-		//p.innerHTML += "<br />"+mesg.getHTMLText();
-		//t.innerHTML += mesg.getDateText();		// Skriver ut texten från arrayen som skapats i funktionen.			
-		//});
-	//	if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
-		//	submit.disabled = true;
-		//}
+		runProgram();
 	});
+		input.onkeydown = function (e) {
+		e = e || window.event;
+		var keyCode = e.keyCode || e.which;
+		
+		if(keyCode==13 && !e.shiftKey) {
+		runProgram();} }
+	
+	var runProgram = function()  {
+	div.innerHTML = "";
+	var answer = MessageBoard.init(input.value); // Läser in texten från textrutan och skickar till funktionen "guess"
+	MessageBoard.renderMessages();
+	}
 }
