b33c7f96f20d51c056066afb32273fc8a0c97b2f
mn22nw <mn22nw@student.lnu.se>
2013-12-17 23:49:24 +0100

    jahopp

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -10,40 +10,44 @@ window.onload = function() {
 	submit2:document.querySelector("#submit2"),
 	images: ['0.jpg', '1.jpg', '2.jpg', '3.jpg', 
         '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'],
-	imagesArr: new RandomGenerator.getPictureArray(4,4), 
-	init: function(e) {
-	var board = new RandomGenerator.getPictureArray(4,4);
-	Memory.imagesArr.push(board);
-	},
-
+	imagesArr:[] , 
+	init: function(value1,value2) {
+	Memory.imagesArr = new RandomGenerator.getPictureArray(value1,value2);
+	console.log(Memory.imagesArr);
+	}
 	}
 	var ul = document.querySelector("#container ul");
-	
-	
-	
-	
 
 	// ------------------------------------------------------------------------------
 
 		Memory.submit1.addEventListener("click", function(e){
 		e = e || window.event;
 		e.preventDefault(); 
-		var imagesArr = RandomGenerator.getPictureArray(2,4);
+		Memory.init(2,4);
 		var br = document.createElement("br");
-		
-		for(var i = 0; i < imagesArr.length; i+=1){ 
-		if ( i === 4) 
+	
+		for(var i = 0; i < Memory.imagesArr.length; i+=1){ 
+		if ( i === 4 || i === 8) 
 		ul.appendChild(br);
 		var li = document.createElement("li");
 		var a = document.createElement("a");
 		var img = document.createElement("img");
-		var imgSrc = "pics/"+Memory.images[i];
+		var imgSrc = "pics/"+Memory.images[Memory.imagesArr[i]];
+		console.log(Memory.imagesArr[i]);
 		li.appendChild(a);
 		a.appendChild(img);
 		img.setAttribute("src", imgSrc);
-		//a.setAttribute("href", "http://www.google.se");
-		ul.appendChild(li);}
+		a.setAttribute("href", imgSrc);
+		ul.appendChild(li);
+		a.addEventListener("click", function(e){
+		e = e || window.event;
+		e.preventDefault(); 
+		
+		//console.log(hej);
+		//console.log(valueOfa);
+		});}
 		Memory.submit1.disabled = true;
+		
 		});
 		
 		Memory.submit2.addEventListener("click", function(e){
