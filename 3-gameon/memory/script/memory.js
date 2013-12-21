6c4940e83f067cc379fdcb1292696b09a0c3a815
mn22nw <mn22nw@student.lnu.se>
2013-12-22 00:15:21 +0100

    Jomenvisst!

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -10,13 +10,15 @@ window.onload = function() {
 	submit1:document.querySelector("#submit1"),
 	submit2:document.querySelector("#submit2"),
 	idArray:[],
-	imagesArr:[] , 
+	imagesArr:[], 
+	flippedImages:[],
 	init: function(value1,value2) {
 	Memory.imagesArr = new RandomGenerator.getPictureArray(value1,value2);
 	}
 	};
 	var ul = document.querySelector("#container ul");
-
+	var score = document.querySelector("#container #score"); 
+	score.innerHTML = "Score: ";
 	// ------------------------------------------------------------------------------
 		Memory.submit1.addEventListener("click", function(e){
 		e = e || window.event;
@@ -45,13 +47,13 @@ window.onload = function() {
 		var atags = document.querySelectorAll("#container ul a");
 		console.log(atags);
 		
+		var countScore=0;
 		var initAtags = function(n) { //console.log("i am image number" +n);
 				return n;
 			};
-		var atagId;
+
 		for (var i = 0; i < atags.length; i+=1){
 		atags[i] = initAtags(i);	
-		atagId= atags[i].getAttribute("data-id");
 		var counting = 0;
 		atags[i].addEventListener("click", function(e){
 		e = e || window.event;
@@ -59,29 +61,39 @@ window.onload = function() {
 		flipBadge(e.target);
 		Memory.idArray.push(e.target.parentNode.getAttribute("data-id"));
 		counting +=1;
-		if (counting === 2){
+		if (counting === 2){ 
+		//e.target.setAttribute("src", "pics/qm.jpg");
 		console.log(Memory.idArray[0], Memory.idArray[1]);
-		console.log("counting 2"); compareBricks(); 
+		console.log("counting 2"); compareBricks(e.target); Memory.idArray.length = 0;
+		
+		
 		counting = 0;}
 		
-	//	if (idAtag 
+
 		});}
 		
-		function compareBricks(){
+		function compareBricks(target){
 		
 		if (Memory.idArray[0] === Memory.idArray[1]){
-		console.log("Rumple");
-		Memory.idArray.length = 0;}
+		Memory.flippedImages.length = 0;
+		score.innerHTML = (countScore +=1); 
+		}
+		
+		else{
+		setTimeout (function() {
+		
+		Memory.flippedImages[0].setAttribute("src", "pics/qm.jpg");	
+		Memory.flippedImages[1].setAttribute("src", "pics/qm.jpg");	
+		Memory.flippedImages.length = 0;
+		}, 1000);
+		}
 		
 		};
 		
 		
-		function flipBadge(target){
-		var idAtag = target.parentNode.getAttribute("data-id");
-		
-		//if (idAtag == 3)
-		//console.log("bard"); 
-			target.setAttribute("src", "pics/"+target.parentNode.getAttribute("data-id")+".jpg");
+		function flipBadge(target){ //push target to img array
+		Memory.flippedImages.push(target);
+		target.setAttribute("src", "pics/"+target.parentNode.getAttribute("data-id")+".jpg");
 		};
 
 		
