dafa19525ccee0db980353123fc5f44564b5b52f
mn22nw <mn22nw@student.lnu.se>
2013-12-26 22:54:01 +0100

    fixar lite

--- 4-forms/script/validator.js
+++ 4-forms/script/validator.js
@@ -6,6 +6,7 @@ window.onload = function() {
 	NodeList.prototype.forEach = Array.prototype.forEach;
 	}
 	
+	var r = "RUMPLE";
 	
 	
 	//st.replaceAll("\\s+","")
@@ -17,12 +18,13 @@ window.onload = function() {
 	errorPostc:document.querySelector(".errorm #error3"),
 	errorEpost:document.querySelector(".errorm #error4"),
 	errorPricem:document.querySelector(".errorm #error5"),
+	submit:document.querySelector("#submitknapp"),
 	idArray:[],
 	init: function(value1,value2) {
 	}};
 	
-	var postCodeReg = /^(\d{3})-(\d{2})$/;//funkar!
-	var postCodeReg2 =  /^\d{5}$/;   //funkar!
+	//var postCodeReg2 = /^(\d{3})-(\d{2})$/;//funkar!
+	var postCodeReg =  /^\d{5}$/;   //funkar!
 	var form = Form.formId;
 	var fn = form.elements["0"];
 	var ln = form.elements["1"];
@@ -33,99 +35,36 @@ window.onload = function() {
 	// fn.style.visibility= "hidden"; //tar bort första elementet
 	fn.focus();
 	
-	/*var str = "Hello world!";
-	var res = str.slice(1,5);*/
+	// -----Name and Last name----- //
+	fn.onblur = function () {
+	Form.errorFn.innerHTML = "";
+	if (fn.value ==="")
 	Form.errorFn.innerHTML = "Detta fält får inte <br />lämnas tomt!";
+	}
+	ln.onblur = function () {
+	Form.errorLn.innerHTML = "";
+	if (ln.value ==="")
+	Form.errorLn.innerHTML = "Detta fält får inte <br />lämnas tomt!";
+	}
+	
+	postc.onblur = function () {
+	Form.errorPostc.innerHTML = "";
+	if (postc.value ==="")
+	Form.errorPostc.innerHTML = "Detta fält får inte <br />lämnas tomt!";
+	}
 	form.onsubmit = function (e) {
-	if(postCodeReg.test(postc.value) ===false){
-	if(postCodeReg2.test(postc.value) === true){
+	// -----Postcode----- //
+	
+	Form.submit.disabled = true;
+	if(postCodeReg.test(postc.value) ===true){
 	var pcArray = postc.value.split("");
-	postc.value = (pcArray[0]+pcArray[1]+pcArray[2]+"-"+pcArray[3]+pcArray[4]);
+	postc.value = (pcArray[0]+pcArray[1]+pcArray[2]+" "+pcArray[3]+pcArray[4]);
+	console.log(postc.value);
 	}
-	else console.log(postc.value);
-	}
-	//submit.disabled = true;	
+	console.log(postc.value);	
 	return false;
 	};
+	};
 	
-	//score.innerHTML = "Poäng: ";
-	// ------------------------------------------------------------------------------
-		/*Memory.submit1.addEventListener("click", function(e){
-		e = e || window.event;
-		e.preventDefault(); 
-		window.scrollTo(0,500);
-		Memory.submit1.disabled = true;	
-		Memory.submit2.disabled = false;
-		generateBricks(2,4);
-		});
-		
-		Memory.submit2.addEventListener("click", function(e){
-		e = e || window.event;
-		e.preventDefault(); 
-		window.scrollTo(0,500);
-		Memory.submit2.disabled = true;	
-		Memory.submit1.disabled = false;	
-		generateBricks(4,4);
-		});
-		
-		function generateBricks(rows,cols){
-		score.innerHTML = "Poäng: ";
-		Memory.init(rows,cols);
-		tries.innerHTML ="";
-		ul.innerHTML ="";
-		var br = document.createElement("br");
-		
-		for(var i = 0; i < Memory.imagesArr.length; i+=1){ 
-
-		if ( i === 4 || i === 8 || i === 12 ) {
-		ul.appendChild(br);}
-		
-		var li = document.createElement("li");
-		var a = document.createElement("a");
-		var img = document.createElement("img");
-		img.setAttribute("src", "pics/qm.jpg");		
-		a.appendChild(img);	
-		li.appendChild(a);
-		a.setAttribute("data-id", Memory.imagesArr[i]);
-		a.setAttribute('href', "#");
-		ul.appendChild(li);
-		} //här slutar for-loopen
-				
-		var atags = document.querySelectorAll("#container ul a");
-		console.log(atags);
-		var countTries = 0;
-		var countScore = 0;
-		var initAtags = function(n) {
-				return n;
-			};
-
-		for (var i = 0; i < atags.length; i+=1){
-		atags[i] = initAtags(i);	
-		var countingBricks = 0;
-		
-		atags[i].addEventListener("click", function(e){
-		e = e || window.event;
-		console.log("hej");
-		e.preventDefault();  
-		flipBadge(e.target); //verkar inte fungera med e.target?
-		Memory.idArray.push(e.target.parentNode.getAttribute("data-id"));
-		countingBricks +=1;
-		if (countingBricks === 2){ 
-		compareBricks(e.target); Memory.idArray.length = 0;
-		countingBricks = 0;}
-		});
-		//FUNKAR INTE MED ENTER?!
-		/*atags[i].addEventListener("click", function(e) {
-		var key = e.which || e.keyCode;
-		if (key === 13)  console.log("Hey there");
-		e = e || window.event; 
-		e.preventDefault();  
-		flipBadge(e.target);
-		Memory.idArray.push(e.target.parentNode.getAttribute("data-id"));
-		countingBricks +=1;
-		if (countingBricks === 2){ 
-		compareBricks(e.target); Memory.idArray.length = 0;
-		countingBricks = 0;}}
-		});*/ };
 
 //http://stackoverflow.com/questions/15643842/createelement-inside-a-for-loop-just-replaces-created-item
