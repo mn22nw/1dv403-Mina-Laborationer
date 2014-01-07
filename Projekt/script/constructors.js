1b8a7bc79a86b3e82d465a2198ec324afdbdb526
mn22nw <mn22nw@student.lnu.se>
2014-01-07 21:23:41 +0100

    fixar mera

--- Projekt/script/constructors.js
+++ Projekt/script/constructors.js
@@ -11,12 +11,70 @@ this.setUrl(url);
 Icon.prototype.createIcon = function() {
 
 	var taskbar = document.querySelector('#taskbar');
-	var icon = document.createElement('div');
-	//var img = 	document.createElement("img");
-	//img.setAttribute("src", "pics/icon1.png");	
+	var icon = document.createElement('a');
+	icon.setAttribute("href", "#");
+	icon.addEventListener("click", function(e){
+	e.preventDefault();
+	//changeBackground.call(desktop,"url('pics/taskbar.jpg')"); 
+	});
 	icon.className = "icon";
-	//icon.appendChild(img);
 	taskbar.appendChild(icon);
-	
+	var desktop = document.querySelector("#page");
 
+
+};
+
+function PopUpFoundation(width, height) {
+	this.width = width;
+	this.height = height;
+
+};
+
+PopUpFoundation.prototype.render = function(){
+
+	//här skrivs popupfönstret ut och skapar elementen 
+	var container = document.querySelector("#container");
+	
+	// ---Popup--- //
+    var popup = document.createElement('div');
+    popup.className = 'popup';
+	popup.style.width = this.width; 
+	popup.style.height = this.height; 
+	console.log(popup.style.height);
+	
+	//---Header---//
+	 var header = document.createElement('div');
+	 header.className = 'headerbar';	 
+	
+	//---Loadbar---//
+	var loadBar = document.createElement('div');
+    loadBar.className = 'load';
+	
+	
+	//---Exit---//
+    var exitButton = document.createElement('div');
+    exitButton.className = 'exitButton';
+    exitButton.onclick = function (e) { 
+	popup.parentNode.removeChild(popup) 
+	loadBar.parentNode.removeChild(loadBar) };	
+	
+	
+	popup.appendChild(exitButton);
+	popup.appendChild(loadBar);
+	container.appendChild(popup);
+};
+
+
+
+//Med denna kan jag skapa flera fönster ju (som en mall)och den ärver från popup foundation //
+function PopUpImages () {   
+	PopUpFoundation.call(this,300,400);
+};
+
+//PopUp ärver från PopUpFoundation
+PopUpImages.prototype = new PopUpFoundation();
+
+//lägg till nya funktioner på PopUp prototype
+PopUpImages.prototype.somethingsomething = function(){
+// om den behöver ha egna egenskaper
 };
--- Projekt/script/script.js
+++ Projekt/script/script.js
@@ -1,32 +1,46 @@
 "use strict";
+window.onload = function() {
 
-window.onload = function(){
+	function changeBackground(url) {   //kanske byt placering på denna dårå
+		this.style.backgroundImage = url;	
+	}
+	// skapar namnrymd (tomt objekt som man fyller på med funktioner man vill nyttja)
+	var WEBDESK = WEBDESK || {};
+	
+	WEBDESK.util = WEBDESK.util || {};
+	
+	WEBDESK.util.popUpImg = Icon.prototype.createIcon();
+	
+	var windowMaker = {
+		run: function() { 
+		var icon1 = new Icon("pics/icon.png");
+		icon1.createIcon();
+	
+	WEBDESK.util.popUpImg();
+	
+	
+		var popUpWindow = document.querySelector('#window'); // element to make resizable
+		var div = document.querySelector('#window');
 
-	var icon1 = new Icon("pics/icon.png");
-	icon1.createIcon();
-
-	var popUpWindow = document.querySelector('#window'); // element to make resizable
-	var div = document.querySelector('#window');
-
-popUpWindow.addEventListener('click', function init() {
+	popUpWindow.addEventListener('click', function init() {
 	/*p.removeEventListener('click', init, false);
     p.className = p.className + ' resizable';
     var resizer = document.createElement('div');
     resizer.className = 'resizer';
     p.appendChild(resizer);*/
     popUpWindow.addEventListener('mousedown', initDrag, false);    //måste ske direkt på mousedown!!
-}, false);
+	}, false);
 
-var startX, startY, startWidth, startHeight;
+	var startX, startY, startWidth, startHeight;
 
-function initDrag(e) {
-   startX = e.clientX;
-   startY = e.clientY;
-   startWidth = parseInt(document.defaultView.getComputedStyle(popUpWindow).width, 10);
-   startHeight = parseInt(document.defaultView.getComputedStyle(popUpWindow).height, 10);
-   document.documentElement.addEventListener('mousemove', doDrag, false);
-   document.documentElement.addEventListener('mouseup', stopDrag, false);
-}
+	function initDrag(e) {
+	   startX = e.clientX;
+	   startY = e.clientY;
+	   startWidth = parseInt(document.defaultView.getComputedStyle(popUpWindow).width, 10);
+	   startHeight = parseInt(document.defaultView.getComputedStyle(popUpWindow).height, 10);
+	   document.documentElement.addEventListener('mousemove', doDrag, false);
+	   document.documentElement.addEventListener('mouseup', stopDrag, false);
+	}
 
 function doDrag(e) {
    popUpWindow.style.width = (startWidth + e.clientX - startX) + 'px';
@@ -38,6 +52,9 @@ function stopDrag(e) {
     document.documentElement.removeEventListener('mouseup', stopDrag, false);
 }
 
-
-
+		} 
+			
+	} 
+	  
+	
 };
