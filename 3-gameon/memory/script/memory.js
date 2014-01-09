d75f721db1ffa80b9ca8f4f203977a7f320a7585
mn22nw <mn22nw@student.lnu.se>
2014-01-09 10:18:28 +0100

    gameon

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -16,16 +16,18 @@ window.onload = function() {
 	Memory.imagesArr = new RandomGenerator.getPictureArray(value1,value2);
 	}
 	};
+	
 	var ul = document.querySelector("#container ul");
 	var score = document.querySelector("#container #score"); 
 	var tries = document.querySelector("#container #tries");
 	var mask = document.querySelector("#container #mask");
 	score.innerHTML = "Poäng: ";
+	
 	// ------------------------------------------------------------------------------
 		Memory.submit1.addEventListener("click", function(e){
 		e = e || window.event;
 		e.preventDefault(); 
-		window.scrollTo(0,500);
+		window.scrollTo(0,300);
 		Memory.submit1.disabled = true;	
 		Memory.submit2.disabled = false;
 		generateBricks(2,4);
@@ -34,7 +36,7 @@ window.onload = function() {
 		Memory.submit2.addEventListener("click", function(e){
 		e = e || window.event;
 		e.preventDefault(); 
-		window.scrollTo(0,500);
+		window.scrollTo(0,350);
 		Memory.submit2.disabled = true;	
 		Memory.submit1.disabled = false;	
 		generateBricks(4,4);
@@ -45,12 +47,9 @@ window.onload = function() {
 		Memory.init(rows,cols);
 		tries.innerHTML ="";
 		ul.innerHTML ="";
-		var br = document.createElement("br");
+		
 		
 		for(var i = 0; i < Memory.imagesArr.length; i+=1){ 
-
-		if ( i === 4 || i === 8 || i === 12 ) {
-		ul.appendChild(br);}
 		
 		var li = document.createElement("li");
 		var a = document.createElement("a");
@@ -59,33 +58,47 @@ window.onload = function() {
 		a.appendChild(img);	
 		li.appendChild(a);
 		a.setAttribute("data-id", Memory.imagesArr[i]);
-		a.setAttribute('href', "#");
+		a.href = "#";
 		ul.appendChild(li);
 		} //här slutar for-loopen
 				
-		var atags = document.querySelectorAll("#container ul a");
-		console.log(atags);
-		var countTries = 0;
+		var atags = document.querySelectorAll("#container ul a");  // array med a-taggarna
+		var countTries = 0; 
 		var countScore = 0;
 		var initAtags = function(n) {
 				return n;
 			};
-
+	//NodeList doesn't have an indexed property setter. ??
 		for (var i = 0; i < atags.length; i+=1){
 		atags[i] = initAtags(i);	
 		var countingBricks = 0;
-		
-		atags[i].addEventListener("click", function(e){
-		e = e || window.event;
-		console.log("hej");
-		e.preventDefault();  
-		flipBadge(e.target); //verkar inte fungera med e.target?
+		};		
+		atags.forEach( function (a) {	 
+		a.addEventListener("click", function(e){
+		e = e || window.event; 
+		e.preventDefault();
+		console.log(a.firstChild);
+		flipBadge(a.firstChild); //verkar inte fungera med e.target?
 		Memory.idArray.push(e.target.parentNode.getAttribute("data-id"));
 		countingBricks +=1;
 		if (countingBricks === 2){ 
-		compareBricks(e.target); Memory.idArray.length = 0;
+		compareBricks(a.firstChild); Memory.idArray.length = 0;
 		countingBricks = 0;}
 		});
+		  
+		a.addEventListener("click", function(e) {
+		var key = e.which || e.keyCode;
+		if (key !== 13)  console.log("Hey there");
+		var x = 0;
+		if(e.keyCode == 38)
+         x = -1; 
+		 
+		else if(e.keyCode == 40)
+         x = 1;
+		});
+		});
+		
+		
 		//FUNKAR INTE MED ENTER?!
 		/*atags[i].addEventListener("click", function(e) {
 		var key = e.which || e.keyCode;
@@ -98,7 +111,7 @@ window.onload = function() {
 		if (countingBricks === 2){ 
 		compareBricks(e.target); Memory.idArray.length = 0;
 		countingBricks = 0;}}
-		});*/ }
+		});*/ 
 		
 		function compareBricks(target){
 		
@@ -131,6 +144,7 @@ window.onload = function() {
 		Memory.flippedImages.push(target);
 		target.setAttribute("src", "pics/"+target.parentNode.getAttribute("data-id")+".jpg");
 		}; 
+		
 		};
 			
 }
